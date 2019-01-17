085e9b42c225e5cc373b5b2cce4e41e17bf9f38a
Robert Lange <robert.lange@barcelonagse.eu>
2019-01-17 10:48:51 +0100

    Log 17/01/19a - Env rendering and reset

--- gym_swarm/envs/swarm_env.py
+++ gym_swarm/envs/swarm_env.py
@@ -2,8 +2,13 @@ import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 
+import os
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.cbook import get_sample_data
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+fish_img = plt.imread(get_sample_data(dir_path + "/images/fish_tropical.png"))
 
 
 class SwarmEnv(gym.Env):
@@ -18,7 +23,7 @@ class SwarmEnv(gym.Env):
                                            self.obs_space_size))
 
         self.current_state = dict(zip(np.arange(self.num_agents),
-                                      [np.empty(2)]*len(keys)))
+                                      [np.empty(2)]*self.num_agents))
 
         self.random_placement = True
         self.done = None
@@ -50,18 +55,15 @@ class SwarmEnv(gym.Env):
 
         # Asynchronous execution of moves - if collision random move instead
         # Implement border conditions - agent enter at opposite ends again
+
+        states_temp = self.current_state.copy()
         for i in self.current_state.keys():
-            temp = self.current_state[i] + move[i]
-            # x-Axis turnover
-            if temp[0] > (self.obs_space_size - 1):
-                temp[0] -= 0
-            elif temp[0] < 0:
-                temp[0] = self.obs_space_size - 1
-            # y-Axis turnover
-            if temp[1] > (self.obs_space_size - 1):
-                temp[1] -= 0
-            elif temp[1] < 0:
-                temp[1] = self.obs_space_size - 1
+            states_temp[i] = self.step_agent(i, move[i])
+            # Check for collision with previous movers
+            if i > 0:
+                while self.invalid_position(states_temp, i+1):
+                    random_action = np.random.randint(8, size=1)
+                    states_temp[i] = self.step_agent(i, action_to_move[random_action])
 
         reward = self.swarm_reward()
 
@@ -72,31 +74,48 @@ class SwarmEnv(gym.Env):
             # For each agent sample an initial placement in grid - uniform!
             states_temp = np.random.randint(self.obs_space_size,
                                             size=(2, self.num_agents))
-            invalid, state_overlap = self.invalid_position(states_temp)
+
+            invalid = self.invalid_position(states_temp, self.num_agents)
 
             while invalid:
-                invalid = self.invalid_position(states_temp)
                 states_temp = np.random.randint(self.obs_space_size,
                                                 size=(2, self.num_agents))
+                invalid = self.invalid_position(states_temp, self.num_agents)
+
 
         # Transform valid state array into dictionary
         self.current_state = dict(enumerate(states_temp.T))
         self.done = False
         return self.current_state
 
-    def invalid_position(self, states_temp):
-        state_overlap = np.zeros((self.num_agents, self.num_agents))
-        for i in range(self.num_agents):
-            check_idx = np.where(state_overlap[i, :] == 0)
-            for j in check_idx:
-                if np.array_equal(states_temp[:, i], states_temp[:, j]):
+    def invalid_position(self, states_temp, num_agents):
+        state_overlap = np.zeros((num_agents, num_agents))
+        for i in range(num_agents):
+            check_idx = np.where(state_overlap[i, :] == 0)[0]
+            for j in range(len(check_idx)):
+                if np.array_equal(states_temp[:, i],
+                                  states_temp[:, check_idx[j]]):
                     state_overlap[i, j] = 1
                     state_overlap[j, i] = 1
 
-        if np.sum(state_overlap) == 0:
+        if np.sum(state_overlap) == num_agents:
             return False
         else:
             return True
+
+    def step_agent(self, i, move_agent):
+        temp = self.current_state[i] + move_agent
+        # x-Axis turnover
+        if temp[0] > (self.obs_space_size - 1):
+            temp[0] -= 0
+        elif temp[0] < 0:
+            temp[0] = self.obs_space_size - 1
+        # y-Axis turnover
+        if temp[1] > (self.obs_space_size - 1):
+            temp[1] -= 0
+        elif temp[1] < 0:
+            temp[1] = self.obs_space_size - 1
+        return temp
 
     def swarm_reward():
         return 0
@@ -105,12 +124,29 @@ class SwarmEnv(gym.Env):
         return
 
     def render(self, mode='human', close=False):
-        fig, ax = plt.subplots()
-        ax.imshow(self.observation_space)
+        x = [self.current_state[state][0] for state in self.current_state]
+        y = [self.current_state[state][1] for state in self.current_state]
 
-        for i in range(self.num_agents):
+        fig, ax = plt.subplots(dpi=200)
+        x_ax = np.linspace(0, self.obs_space_size-1)
+        y_ax = np.linspace(0, self.obs_space_size-1)
+        plot = ax.plot(x_ax, y_ax, linestyle="")
 
-        return
+        ax_width = ax.get_window_extent().width
+        fig_width = fig.get_window_extent().width
+        fig_height = fig.get_window_extent().height
+        fish_size = 0.25*ax_width/(fig_width*len(x))
+        fish_axs = [None for i in range(len(x))]
+
+        for i in range(len(x)):
+            loc = ax.transData.transform((x[i], y[i]))
+            fish_axs[i] = fig.add_axes([loc[0]/fig_width-fish_size/2,
+                                        loc[1]/fig_height-fish_size/2,
+                                        fish_size, fish_size], anchor='C')
+            fish_axs[i].imshow(fish_img)
+            fish_axs[i].axis("off")
+
+        plt.show()
 
     def set_env_parameters(self, num_agents=4,
                            obs_space_size=20, verbose=True):
@@ -141,7 +177,3 @@ ACTION_LOOKUP = {0: "left",
                  5: "right-up",
                  6: "up",
                  7: "left-up"}
-
-
-if __name__ == "__main__":
-    env = SwarmEnv()
